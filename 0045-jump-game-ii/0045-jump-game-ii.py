eb5d18f50dbb4c71a941685e41c7f2f7ce464c84
Ishan-Kumar9 <ishanraj2025@gmail.com>
2026-03-27 15:00:09 +0530

    Time: 4 ms (69.47%), Space: 20.1 MB (81.77%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        total = 0
-        curr = 0
+        smallest = 0
+        n = len(nums)
+        end, far = 0, 0
 
-        for i in range(len(nums)-1):
-            total = max(total, i + nums[i])
-            if i == curr:
-                jumps += 1
-                curr = total
-
-        return jumps
+        for i in range(n-1):
+            far = max(far, i+nums[i])
+            if i == end:
+                smallest += 1
+                end = far
+            
+        return smallest
